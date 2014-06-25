0e6dd5b09ba8e97439a1753a27807304a0f88303
nir0s <nir36g@gmail.com>
2014-06-25 12:42:22 +0300

    final addition before builds commence

--- vagrant/cloudify-components/provision.sh
+++ vagrant/cloudify-components/provision.sh
@@ -1,8 +1,18 @@
-echo bootstrapping packman...
+
+# echo bootstrapping packman...
 
 # update and install prereqs
 sudo apt-get -y update &&
-sudo apt-get install -y python-setuptools python-dev rubygems rpm &&
+sudo apt-get install -y python-setuptools python-dev rubygems rpm libyaml-dev &&
+
+# install ruby
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p547.tar.bz2
+tar -xjf ruby-1.9.3-p547.tar.bz2
+cd ruby-1.9.3-p547
+./configure --disable-install-doc
+make
+sudo make install
+cd ~
 
 # install fpm and configure gem/bundler
 sudo gem install fpm --no-ri --no-rdoc &&
@@ -13,6 +23,8 @@ echo -e 'gem: --no-ri --no-rdoc\ninstall: --no-rdoc --no-ri\nupdate:  --no-rdoc 
 sudo apt-get purge pip
 sudo easy_install -U pip &&
 sudo wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python &&
+# install pip alternative
+# curl --silent --show-error --retry 5 https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py | sudo python
 
 # install virtualenv
 sudo pip install virtualenv==1.11.4 &&
@@ -21,11 +33,11 @@ sudo pip install virtualenv==1.11.4 &&
 sudo pip install https://github.com/cloudify-cosmo/packman/archive/develop.tar.gz
 
 # create cloudify components package
-cd /home/vagrant/cloudify-packager/ &&
+cd /cloudify-packager/ &&
 sudo pkm make -c logstash,elasticsearch,kibana3,nginx,rabbitmq-server,riemann,nodejs,openjdk-7-jdk,virtualenv,curl,make,ruby,workflow_gems -v &&
 sudo pkm pack -c cloudify-components
-sudo mkdir /home/vagrant/cloudify-packager/output-packages
-sudo mv /cloudify/* /home/vagrant/cloudify-packager/output-packages
+sudo mkdir /cloudify-packager/output-packages
+sudo mv /cloudify/* /cloudify-packager/output-packages
 
 # TODO: add virtualenv to provisioning process
 # sudo pip install virtualenvwrapper
