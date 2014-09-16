97cb8e71824c3ea8566c89caf570a9b91d4d8191
adamlavie <adaml@gigaspaces.com>
2014-09-16 13:53:01 +0300

    CFY-1308 added init and config scripts

--- docker/vars.py
+++ docker/vars.py
@@ -7,53 +7,64 @@ VARS = {
         "repository": "ubuntu",
         "tag": "12.04"
     },
-    "dependencies": [
-        "git",
-        "make",
-        "curl"
-    ],
     "rabbitmq": {
-    	"package_url": "http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.4/rabbitmq-server_3.2.4-1_all.deb",
-    	"package_dest": "/opt/tmp/rabbitmq/rabbitmq-server.deb"
+        "package_url": "http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.4/rabbitmq-server_3.2.4-1_all.deb",
+        "package_dest": "/opt/tmp/rabbitmq/rabbitmq-server.deb"
     },
     "riemann": {
-    	"package_url": "http://aphyr.com/riemann/riemann_0.2.6_all.deb",
-    	"package_dest": "/opt/tmp/riemann/riemann.deb",
-    	"langohr_url": "https://s3-eu-west-1.amazonaws.com/gigaspaces-repository-eu/langohr/2.11.0/langohr.jar",
-    	"langohr_dest": "/usr/lib/riemann/langohr.jar"
+        "package_url": "http://aphyr.com/riemann/riemann_0.2.6_all.deb",
+        "package_dest": "/opt/tmp/riemann/riemann.deb",
+        "langohr_url": "https://s3-eu-west-1.amazonaws.com/gigaspaces-repository-eu/langohr/2.11.0/langohr.jar",
+        "langohr_dest": "/usr/lib/riemann/langohr.jar"
     },
     "python": {
-    	"package_url": "https://bootstrap.pypa.io/get-pip.py",
-    	"virtualenv_version": "1.11.4"
+        "package_url": "https://bootstrap.pypa.io/get-pip.py",
+        "virtualenv_version": "1.11.4"
     },
     "nodejs": {
-    	"repo_name": "ppa:chris-lea/node.js"
+        "repo_name": "ppa:chris-lea/node.js"
     },
     "logstash": {
-    	"package_url": "https://download.elasticsearch.org/logstash/logstash/logstash-1.3.2-flatjar.jar",
-    	"package_dest": "/opt/tmp/logstash/logstash.jar"
-    }, 
+        "service_name": "logstash",
+        "package_url": "https://download.elasticsearch.org/logstash/logstash/logstash-1.3.2-flatjar.jar",
+        "package_dest": "/opt/tmp/logstash/logstash.jar",
+        "init_file": {
+            "init_file_path": "/opt/tmp/init/logstash.conf",
+            "jar": "logstash.jar",
+            "log_file": "/var/log/logstash.out",
+            "conf_path": "/etc/logstash.conf",
+            "run_dir": "/opt/logstash",
+            "user": "root",
+        },
+        "conf_file": {
+            "conf_file_path": "/opt/tmp/conf/logstash.conf",
+            "logs_queue": "cloudify-logs",
+            "events_queue": "cloudify-events",
+            "events_index": "cloudify_events",
+            "test_tcp_port": "9999",
+        }
+    },
     "elasticsearch": {
-    	"package_url": "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.tar.gz",
-    	"package_dest": "/opt/tmp/elasticsearch/elasticsearch.tar.gz",
-    	"untar_dest": "/opt/",
-    	"sim_link": "/opt/elasticsearch"
+        "package_url": "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.tar.gz",
+        "package_dest": "/opt/tmp/elasticsearch/elasticsearch.tar.gz",
+        "untar_dest": "/opt/",
+        "sim_link": "/opt/elasticsearch"
     },
     "kibana": {
-    	"package_url": "https://download.elasticsearch.org/kibana/kibana/kibana-3.0.0milestone4.tar.gz",
-    	"package_dest": "/opt/tmp/kibana.tar.gz",
-    	"untar_dest": "/opt/",
-    	"sim_link": "/opt/kibana"
+        "package_url": "https://download.elasticsearch.org/kibana/kibana/kibana-3.0.0milestone4.tar.gz",
+        "package_dest": "/opt/tmp/kibana.tar.gz",
+        "untar_dest": "/opt/",
+        "sim_link": "/opt/kibana"
     },
     "influxdb": {
-    	"package_url": "http://s3.amazonaws.com/influxdb/influxdb_0.8.0_amd64.deb",
-    	"package_dest": "/opt/tmp/influxdb.deb"
+        "package_url": "http://s3.amazonaws.com/influxdb/influxdb_0.8.0_amd64.deb",
+        "package_dest": "/opt/tmp/influxdb.deb"
     },
     "celery": {
-    	"package_url": "https://github.com/cloudify-cosmo/cloudify-manager/archive/master.tar.gz",
-    	"package_dest": "/opt/tmp/celery/manager.tar.gz",
-    	"untar_dest": "/opt/celery/cloudify.management__worker/env",
-    	"cloudify_rest_client_url": "https://github.com/cloudify-cosmo/cloudify-rest-client/archive/master.tar.gz",
-    	"plugins_common_url": "https://github.com/cloudify-cosmo/cloudify-plugins-common/archive/master.tar.gz"
+        "package_url": "https://codeload.github.com/cloudify-cosmo/cloudify-manager/tar.gz/master",
+        "package_dest": "/opt/tmp/celery/manager.tar.gz",
+        "untar_dest": "/opt/celery/cloudify.management__worker/env",
+        "cloudify_rest_client_url": "https://github.com/cloudify-cosmo/cloudify-rest-client/archive/master.tar.gz",
+        "plugins_common_url": "https://github.com/cloudify-cosmo/cloudify-plugins-common/archive/master.tar.gz"
     }
 }
