c8e46cef527d5097556c36a9c33fe82935d01158
adamlavie <adaml@gigaspaces.com>
2014-09-22 17:36:06 +0300

    CFY-1308 work on elasticsearch and nginx

--- docker/utils/bootstrap_utils.sh
+++ docker/utils/bootstrap_utils.sh
@@ -52,6 +52,20 @@ function check_file
 	fi
 }
 
+function wait_for_port
+{
+    c=0
+    while ! echo exit | curl http://localhost:$1;
+    do
+            if [[ $c -gt 24 ]]; then
+                    state_error "failed to connect to elasticsearch..."
+            fi
+            echo "elasticsearch host not up yet... retrying... ($c/24)"
+            sleep 5;
+            c=$((c+1))
+    done
+}
+
 function check_upstart
 {
 	echo "checking to see if $1 daemon is running..."
