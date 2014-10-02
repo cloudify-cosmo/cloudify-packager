727df87c083cd5ea79595d75b1383a9b60afae79
adamlavie <adaml@gigaspaces.com>
2014-10-02 20:47:37 +0300

    CFY-1308 normalized rest-service startup.

--- docker/manager/run_manager.sh
+++ docker/manager/run_manager.sh
@@ -2,7 +2,13 @@
 # environment vars be injected by docker:
 #   MANAGER_REST_CONFIG_PATH - path to the manager virtualenv
 #   MANAGER_VIRTUALENV_DIR - path to the manager virtualenv
+#   SERVER_FILES_DIR - path to server.py main entry
 
 export MANAGER_REST_CONFIG_PATH=$MANAGER_REST_CONFIG_PATH
+# calculate number of workers according to number of cores. (1-1 ratio)
 WORKERS=$(($(nproc)*2+1))
-$MANAGER_VIRTUALENV_DIR/bin/gunicorn  -w ${WORKERS} -b 0.0.0.0:8100 --timeout 300 server:app
+
+# run the server service using predefined virtual env
+source $MANAGER_VIRTUALENV_DIR/bin/activate
+pushd $SERVER_FILES_DIR
+gunicorn  -w ${WORKERS} -b 0.0.0.0:8100 --timeout 300 server:app
