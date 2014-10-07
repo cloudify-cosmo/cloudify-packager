70c7419bc65ae1acd9ce0105a15e303dd89351c6
nir0s <nir36g@gmail.com>
2014-10-07 18:26:23 +0300

    think done. awaiting limor's approval

--- vagrant/cloudify-core/provision.sh
+++ vagrant/cloudify-core/provision.sh
@@ -1,51 +1,110 @@
-echo bootstrapping...
+REST_CLIENT_SHA=""
+COMMON_PLUGIN_SHA=""
+MANAGER_SHA=""
+PACKMAN_SHA=""
+SCRIPTS_PLUGIN_SHA=""
+DIAMOND_PLUGIN_SHA=""
+
+# echo bootstrapping packman...
 
 # update and install prereqs
-sudo yum -y update &&
-sudo yum install yum-downloadonly wget mlocate yum-utils python-devel libyaml-devel ruby rubygems ruby-devel -y
+sudo add-apt-repository ppa:git-core/ppa &&
+sudo apt-get -y update &&
+sudo apt-get install -y curl python-dev rubygems rpm git &&
 
-# install fpm
-sudo gem install fpm --no-rdoc --no-ri
+# install ruby
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p547.tar.bz2
+tar -xjf ruby-1.9.3-p547.tar.bz2
+cd ruby-1.9.3-p547
+./configure --disable-install-doc
+make
+sudo make install
+cd ~
 
-# configure gem and bundler
+# install fpm and configure gem/bundler
+sudo gem install fpm --no-ri --no-rdoc &&
 echo -e 'gem: --no-ri --no-rdoc\ninstall: --no-rdoc --no-ri\nupdate:  --no-rdoc --no-ri' >> ~/.gemrc
-echo -e 'gem: --no-ri --no-rdoc\ninstall: --no-rdoc --no-ri\nupdate:  --no-rdoc --no-ri' >> /root/.gemrc
-
-# install python and additions
-# http://bicofino.io/blog/2014/01/16/installing-python-2-dot-7-6-on-centos-6-dot-5/
-sudo yum groupinstall -y 'development tools'
-sudo yum install -y zlib-devel bzip2-devel openssl-devel xz-libs
-sudo mkdir /py27
-cd /py27
-sudo wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
-sudo xz -d Python-2.7.6.tar.xz
-sudo tar -xvf Python-2.7.6.tar
-cd Python-2.7.6
-sudo ./configure --prefix=/usr
-sudo make
-sudo make altinstall
 
 # install pip
-cd /py27
-sudo wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py &&
-sudo /usr/bin/python2.7 get-pip.py &&
-
-# install virtualenv
-sudo /usr/bin/pip2.7 install virtualenv==1.11.4 &&
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
 
 # install packman
-sudo /usr/bin/pip2.7 install https://github.com/cloudify-cosmo/packman/archive/develop.tar.gz
+git clone https://github.com/cloudify-cosmo/packman.git
+pushd packman
+    if [ -n "$PACKMAN_SHA" ]; then
+        git reset --hard $PACKMAN_SHA
+    fi
+    sudo pip install .
+popd
 
-# create cloudify components package
-cd /home/vagrant/cloudify-packager/ &&
+# install virtualenv
+sudo pip install virtualenv==1.11.4 &&
+
+cd /cloudify-packager/ &&
 
 # create package resources
 sudo pkm get -c manager
 sudo pkm get -c celery
 
-# LIMOR, PLEASE COMPLETE THE GET PROCESS HERE
+echo '# GET PROCESS'
 
-# create package
+sudo /opt/celery/bin/pip install celery==3.0.24
+
+git clone https://github.com/cloudify-cosmo/cloudify-dsl-parser.git
+pushd cloudify-dsl-parser
+    if [ -n "$DSL_PARSER_SHA" ]; then
+        git reset --hard $DSL_PARSER_SHA
+    fi
+    sudo /opt/manager/bin/pip install .
+popd
+
+git clone https://github.com/cloudify-cosmo/cloudify-amqp-influxdb.git
+pushd cloudify-amqp-influxdb
+    if [ -n "$AMQP_INFLUX_SHA" ]; then
+        git reset --hard $AMQP_INFLUX_SHA
+    fi
+    sudo /opt/manager/bin/pip install .
+popd
+
+git clone https://github.com/cloudify-cosmo/cloudify-rest-client.git
+pushd cloudify-rest-client
+    if [ -n "$REST_CLIENT_SHA" ]; then
+        git reset --hard $REST_CLIENT_SHA
+    fi
+    sudo /opt/celery/bin/pip install .
+popd
+
+git clone https://github.com/cloudify-cosmo/cloudify-plugins-common.git
+pushd cloudify-plugins-common
+    if [ -n "$COMMON_PLUGIN_SHA" ]; then
+        git reset --hard $COMMON_PLUGIN_SHA
+    fi
+    sudo /opt/celery/bin/pip install .
+popd
+
+git clone https://github.com/cloudify-cosmo/cloudify-manager.git
+pushd cloudify-manager
+    if [ -n "$MANAGER_SHA" ]; then
+        git reset --hard $MANAGER_SHA
+    fi
+    pushd plugins/plugin-installer
+      sudo /opt/celery/bin/pip install .
+    popd
+    pushd plugins/agent-installer
+      sudo /opt/celery/bin/pip install .
+    popd
+    pushd plugins/riemann-controller
+      sudo /opt/celery/bin/pip install .
+    popd
+    pushd workflows
+      sudo /opt/celery/bin/pip install .
+    popd
+    pushd rest-service
+      sudo /opt/manager/bin/pip install .
+    popd
+popd
+
+# create packages
 sudo pkm pack -c manager
 sudo pkm pack -c celery
 sudo pkm pack -c cloudify-core
