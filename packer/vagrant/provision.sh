9c9f00dc2a8ea714983c2c8409ab1762b8a18a35
Nir Cohen <nir0s@users.noreply.github.com>
2014-10-07 12:48:33 +0300

    Update provision.sh

--- packer/vagrant/provision.sh
+++ packer/vagrant/provision.sh
@@ -114,8 +114,8 @@ cp /vagrant/insecure_private_key /home/${USERNAME}/.ssh/cloudify_private_key
 cp cloudify-manager-blueprints/simple/inputs.json.template inputs.json
 sed -i "s|\"public_ip\": \"\"|\"public_ip\": \"127.0.0.1\"|g" inputs.json
 sed -i "s|\"private_ip\": \"\"|\"private_ip\": \"127.0.0.1\"|g" inputs.json
-sed -i "s|\"ssh_user\": \"\"|\"ssh_user\": \"/home/${USERNAME}/.ssh/cloudify_private_key\"|g" inputs.json
-sed -i "s|\"ssh_key_filename\": \"\"|\"ssh_key_filename\": \"${USERNAME}\"|g" inputs.json
+sed -i "s|\"ssh_user\": \"\"|\"ssh_user\": \"${USERNAME}\"|g" inputs.json
+sed -i "s|\"ssh_key_filename\": \"\"|\"ssh_key_filename\": \"/home/${USERNAME}/.ssh/cloudify_private_key\"|g" inputs.json
 
 # bootstrap the manager locally
 cfy bootstrap -v -p cloudify-manager-blueprints/simple/simple.yaml -i inputs.json &&
