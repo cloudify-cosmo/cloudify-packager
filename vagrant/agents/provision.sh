08afac0698bf8df181bc3b57f95faf3f17fa0fce
nir0s <nir36g@gmail.com>
2014-12-26 22:01:16 +0200

    last fixes

--- vagrant/agents/provision.sh
+++ vagrant/agents/provision.sh
@@ -44,33 +44,23 @@ source agent-packager/bin/activate &&
 
 cd /tmp
 
-# echo cloning cloudify-agent-packager repo
-# git clone https://github.com/cloudify-cosmo/cloudify-agent-packager.git
-# pushd cloudify-agent-packager
-# 	if [ -n "$AGENT_PACKAGER_SHA" ]; then
-# 		git reset --hard $AGENT_PACKAGER_SHA
-# 	fi
-# 	git checkout agent-refactoring-project
-# 	sudo pip install .
-# popd
+# clone modules
+clone "cloudify-agent-packager" "${AGENT_PACKAGER_SHA}"
+clone "cloudify-rest-client" "${REST_CLIENT_SHA}"
+clone "cloudify-plugins-common" "${PLUGINS_COMMON_SHA}"
+clone "cloudify-script-plugin" "${SCRIPTS_PLUGIN_SHA}"
+clone "cloudify-diamond-plugin" "${DIAMOND_PLUGIN_SHA}"
+clone "cloudify-agent-installer-plugin" "${AGENT_INSTALLER_SHA}" "iliapolo"
+clone "cloudify-plugin-installer-plugin" "${PLUGIN_INSTALLER_SHA}" "iliapolo"
+clone "cloudify-windows-agent-installer-plugin" "${WINDOWS_AGENT_INSTALLER_SHA}" "iliapolo"
+clone "cloudify-windows-plugin-installer-plugin" "${WINDOWS_PLUGIN_INSTALLER_SHA}" "iliapolo"
+clone "cloudify-agent" "${CLOUDIFY_AGENT_SHA}" "nir0s"
 
 # install agent packager
-clone cloudify-agent-packager AGENT_PACKAGER_SHA
 pushd cloudify-agent-packager
 	git checkout agent-refactoring-project
 	sudo pip install .
 popd
-
-# clone modules
-clone "cloudify-rest-client" ${REST_CLIENT_SHA}
-clone "cloudify-plugins-common" ${PLUGINS_COMMON_SHA}
-clone "cloudify-script-plugin" ${SCRIPTS_PLUGIN_SHA}
-clone "cloudify-diamond-plugin" ${DIAMOND_PLUGIN_SHA}
-clone "cloudify-agent-installer-plugin" ${AGENT_INSTALLER_SHA} "iliapolo"
-clone "cloudify-plugin-installer-plugin" ${PLUGIN_INSTALLER_SHA} "iliapolo"
-clone "cloudify-windows-agent-installer-plugin" ${WINDOWS_AGENT_INSTALLER_SHA} "iliapolo"
-clone "cloudify-windows-plugin-installer-plugin" ${WINDOWS_PLUGIN_INSTALLER_SHA} "iliapolo"
-clone "cloudify-agent" ${CLOUDIFY_AGENT_SHA} "nir0s"
 
 # create agent
 cfy-ap -c /vagrant/packager.yaml -f -v
