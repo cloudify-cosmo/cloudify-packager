2fb297868fbb7919466323fc3a57a4eb99443d29
nir0s <nir36g@gmail.com>
2014-12-26 21:29:54 +0200

    fixes to provisioning process

--- vagrant/agents/provision.sh
+++ vagrant/agents/provision.sh
@@ -6,8 +6,8 @@ function clone {
 	echo cloning ${REPO} repo
 	git clone https://github.com/${ORG}/${REPO}.git
 	pushd ${REPO}
-		if [ -n "${SHA}" ]; then
-			git reset --hard ${SHA}
+		if [ -n "$SHA" ]; then
+			git reset --hard $SHA
 		fi
 	popd
 }
@@ -61,14 +61,15 @@ pushd cloudify-agent-packager
 popd
 
 # clone modules
-clone cloudify-rest-client REST_CLIENT_SHA
-clone cloudify-plugins-common PLUGINS_COMMON_SHA
-clone cloudify-script-plugin SCRIPTS_PLUGIN_SHA
-clone cloudify-diamond-plugin DIAMOND_PLUGIN_SHA
-clone cloudify-agent-installer-plugin AGENT_INSTALLER_SHA iliapolo
-clone cloudify-plugin-installer-plugin PLUGIN_INSTALLER_SHA iliapolo
-clone cloudify-windows-agent-installer-plugin WINDOWS_AGENT_INSTALLER_SHA iliapolo
-clone cloudify-windows-plugin-installer-plugin WINDOWS_PLUGIN_INSTALLER_SHA iliapolo
+clone cloudify-rest-client ${REST_CLIENT_SHA}
+clone cloudify-plugins-common ${PLUGINS_COMMON_SHA}
+clone cloudify-script-plugin ${SCRIPTS_PLUGIN_SHA}
+clone cloudify-diamond-plugin ${DIAMOND_PLUGIN_SHA}
+clone cloudify-agent-installer-plugin ${AGENT_INSTALLER_SHA} iliapolo
+clone cloudify-plugin-installer-plugin ${PLUGIN_INSTALLER_SHA} iliapolo
+clone cloudify-windows-agent-installer-plugin ${WINDOWS_AGENT_INSTALLER_SHA} iliapolo
+clone cloudify-windows-plugin-installer-plugin ${WINDOWS_PLUGIN_INSTALLER_SHA} iliapolo
+clone cloudify-agent ${CLOUDIFY_AGENT_SHA} nir0s
 
 # create agent
 cfy-ap -c /vagrant/packager.yaml -f -v
