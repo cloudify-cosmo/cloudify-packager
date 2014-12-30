f3a99a193eb2c35be1f392cb164fe7e18f1fcbe1
limor-gs <limor@gigaspaces.com>
2014-12-30 16:41:09 +0200

    Remove docker data creation

--- vagrant/docker_images/provision.sh
+++ vagrant/docker_images/provision.sh
@@ -29,16 +29,12 @@ build_images()
   clone_packager /tmp/cloudify-packager
   echo Building cloudify stack image.
   build_image /tmp/cloudify-packager/docker cloudify:latest
-  echo Building cloudify data image.
-  build_image /tmp/cloudify-packager/docker/data_container data:latest
 }
 
 start_and_export_containers()
 {
   sudo docker run -t --name=cloudify -d cloudify:latest /bin/bash
-  sudo docker run -t -d --name data data /bin/bash
   sudo docker export cloudify > /tmp/coudify-docker_3.1.0-ga-b85.tar
-  sudo docker export data > /tmp/cloudify-docker-data_3.1.0-ga-b85.tar
 }
 
 main() 
