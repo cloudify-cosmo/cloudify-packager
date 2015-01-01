f5ab282c9cbc39ce098a2205ff122601e164c393
adamlavie <adaml@gigaspaces.com>
2015-01-01 23:12:10 +0200

    using no-cache flag to avoid environment issues when using same machine to build the image

--- docker/ubuntu_agent/run_container.sh
+++ docker/ubuntu_agent/run_container.sh
@@ -3,7 +3,7 @@
 #remove image
 sudo docker images | grep ubuntu_agent_build && sudo docker images --no-trunc | grep ubuntu_agent_build | awk '{print $3}' | xargs sudo docker rmi
 
-sudo docker build -t ubuntu_agent_build .
+sudo docker build -t ubuntu_agent_build --no-cache .
 sudo docker run -t -d --name=ubuntu_agent_build ubuntu_agent_build
 sudo docker cp ubuntu_agent_build:/cloudify/ /
 sudo docker rm -f ubuntu_agent_build
