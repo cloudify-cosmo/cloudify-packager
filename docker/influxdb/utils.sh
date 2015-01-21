8312724369e9cfac4ad46590a8a48135f491e715
nir0s <nir36g@gmail.com>
2015-01-21 15:54:25 +0200

    utils fix

--- docker/influxdb/utils.sh
+++ docker/influxdb/utils.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-#!/usr/bin/env bash
-
-function state_error
-{
-    echo "ERROR: ${1:-UNKNOWN} (status $?)" 1>&2
-    exit 1
-}
-
 function wait_for_port
 {
     c=0
     while ! echo exit | curl http://localhost:$1;
     do
             if [[ $c -gt 24 ]]; then
-                    state_error "failed to connect to service..."
+                echo "ERROR: failed to connect to service... (status $?)" 1>&2
+                exit 1
             fi
             echo "host not up yet... retrying... ($c/24)"
             sleep 5;
