83bfe6c451402e3c82c6df05d2475f14645ea768
Michael Sverdlik <m1keil@users.noreply.github.com>
2015-01-28 12:44:32 +0200

    Update prepare_nightly.sh

--- image-builder/provision/prepare_nightly.sh
+++ image-builder/provision/prepare_nightly.sh
@@ -15,7 +15,15 @@ DEBIAN_FRONTEND=noninteractive sudo -E dpkg-reconfigure cloud-init
 echo manual | sudo tee /etc/init/ttyS0.override
 
 # install guest additions
-sudo apt-get install -y virtualbox-guest-utils
+sudo apt-get install -y module-assistant
+sudo m-a -i prepare
+wget http://download.virtualbox.org/virtualbox/4.3.20/VBoxGuestAdditions_4.3.20.iso
+sudo mkdir -p /mnt/iso
+sudo sudo mount -o loop VBoxGuestAdditions_4.3.20.iso /mnt/iso/
+sudo /mnt/iso/VBoxLinuxAdditions.run
+sudo umount /mnt/iso
+sudo rmdir /mnt/iso
+rm VBoxGuestAdditions_4.3.20.iso
 
 # change hostname
 echo cloudify | sudo -S tee /etc/hostname
