34e39dc2c48a2abf61bfdfcd2a9b6b0182780d76
nir0s <nir36g@gmail.com>
2015-03-26 07:04:45 +0200

    nicer looking echos

--- image-builder/docker/provision.sh
+++ image-builder/docker/provision.sh
@@ -2,17 +2,18 @@
 
 function install_docker
 {
+  echo Installing Docker
   curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 }
 
 function install_docker_compose
 {
-  echo installing docker compose
+  echo Installing docker-compose
   # docker-compose requires requests in version 2.2.1. will probably change.
   sudo pip install requests==2.2.1 --upgrade
   sudo pip install docker-compose==1.1.0
 
-  echo exposing docker api
+  echo Exposing docker api
   sudo /bin/sh -c 'echo DOCKER_OPTS=\"-H tcp://127.0.0.1:4243 -H unix:///var/run/docker.sock\" >> /etc/default/docker'
   sudo restart docker
   export DOCKER_HOST=tcp://localhost:4243
@@ -20,13 +21,13 @@ function install_docker_compose
 
 function install_pip
 {
-  echo installing pip
+  echo Installing pip
   curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
 }
 
 function install_boto
 {
-  echo installing boto
+  echo Installing boto
   sudo pip install boto==2.36.0
 
 }
