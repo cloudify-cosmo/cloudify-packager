1bc5d9f01a02dee4ef0c1fba783ff18dae590a33
nir0s <nir36g@gmail.com>
2015-03-28 10:32:36 +0300

    attempting with not-so-recent version of git

--- image-builder/packman/provision.sh
+++ image-builder/packman/provision.sh
@@ -1,17 +1,16 @@
 function install_prereqs
 {
+    # some of these are not related to the image specifically but rather to processes that will be executed later
+    # such as git, python-dev and make
     if which apt-get; then
         # ubuntu
         sudo apt-get -y update &&
-        . /etc/lsb-release
+        # the commented below might not be required. If the latest version of git is required, we'll have to enable them
         # precise - python-software-properties
         # trusty - software-properties-common
-        if [ "$DISTRIB_RELEASE" == "14.04" ]; then
-            sudo apt-get install -y software-properties-common
-        elif [ "$DISTRIB_RELEASE" == "12.04" ]; then
-            sudo apt-get install -y python-software-properties
-        fi
-        sudo add-apt-repository -y ppa:git-core/ppa &&
+        # sudo apt-get install -y software-properties-common
+        # sudo apt-get install -y python-software-properties
+        # sudo add-apt-repository -y ppa:git-core/ppa &&
         sudo apt-get install -y curl python-dev git make gcc libyaml-dev zlib1g-dev g++
     elif which yum; then
         # centos/REHL
@@ -28,7 +27,9 @@ function install_prereqs
 
 function install_ruby
 {
-    wget http://mirrors.ibiblio.org/ruby/1.9/ruby-1.9.3-rc1.tar.gz --no-check-certificate
+    wget https://ftp.ruby-lang.org/pub/ruby/ruby-1.9.3-rc1.tar.gz --no-check-certificate
+    # alt since ruby-lang's ftp blows
+    # wget http://mirrors.ibiblio.org/ruby/1.9/ruby-1.9.3-rc1.tar.gz --no-check-certificate
     tar -xzvf ruby-1.9.3-rc1.tar.gz
     cd ruby-1.9.3-rc1
     ./configure --disable-install-doc
@@ -48,7 +49,6 @@ function install_pip
 {
     curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
 }
-
 
 install_prereqs &&
 if ! which ruby; then
