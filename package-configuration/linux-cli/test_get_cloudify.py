f43597e8f178fe45f7ee9a05ba7e7923914a38ef
nir0s <nir36g@gmail.com>
2015-04-12 19:38:07 +0300

    some test modifications

--- package-configuration/linux-cli/test_get_cloudify.py
+++ package-configuration/linux-cli/test_get_cloudify.py
@@ -13,30 +13,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ############
-import sys
 import unittest
 import urllib
 import tempfile
 from StringIO import StringIO
 from mock import MagicMock
 
-sys.path.append('../')
-from get_cloudify import InstallerError
-import get_cloudify as cli_builder
+get_cloudify = __import__("get_cloudify")
 
 
 class CliBuilderUnitTests(unittest.TestCase):
     """Unit tests for functions in get_cloudify.py"""
 
     def setUp(self):
-        self.cli_builder = cli_builder
-        self.cli_builder.SUDO = False
-        self.cli_builder.IS_VIRTUALENV = False
+        self.get_cloudify = get_cloudify
+        self.get_cloudify.SUDO = False
+        self.get_cloudify.IS_VIRTUALENV = False
 
     def test_validate_urls(self):
-        self._validate_url(self.cli_builder.PIP_URL)
-        self._validate_url(self.cli_builder.PYCR64_URL)
-        self._validate_url(self.cli_builder.PYCR32_URL)
+        self._validate_url(self.get_cloudify.PIP_URL)
+        self._validate_url(self.get_cloudify.PYCR64_URL)
+        self._validate_url(self.get_cloudify.PYCR32_URL)
 
     def _validate_url(self, url):
         try:
@@ -47,12 +44,12 @@ class CliBuilderUnitTests(unittest.TestCase):
             raise AssertionError('url {} is not valid.'.format(url))
 
     def test_run_valid_command(self):
-        self.cli_builder.VERBOSE = True
+        self.get_cloudify.VERBOSE = True
         builder_stdout = StringIO()
         # replacing builder stdout
-        self.cli_builder.sys.stdout = builder_stdout
-        proc = cli_builder.run('echo just another STDOUT && '
-                               '>&2 echo just another STDERR')
+        self.get_cloudify.sys.stdout = builder_stdout
+        proc = get_cloudify.run('echo just another STDOUT && '
+                                '>&2 echo just another STDERR')
         self.assertIn('STDOUT: just another STDOUT',
                       builder_stdout.getvalue(),
                       'expected stdout was not printed')
@@ -63,13 +60,13 @@ class CliBuilderUnitTests(unittest.TestCase):
         self.assertEqual(proc.returncode, 0, 'process execution failed')
 
     def test_run_invalid_command(self):
-        self.cli_builder.VERBOSE = True
+        self.get_cloudify.VERBOSE = True
         builder_stdout = StringIO()
         # replacing builder stdout
-        self.cli_builder.sys.stdout = builder_stdout
+        self.get_cloudify.sys.stdout = builder_stdout
         cmd = 'this is not a valid command'
         try:
-            cli_builder.run(cmd)
+            get_cloudify.run(cmd)
             self.fail('command \'{}\' execution was expected to fail'
                       .format(cmd))
         except RuntimeError as e:
@@ -79,39 +76,39 @@ class CliBuilderUnitTests(unittest.TestCase):
     def test_install_failed_download(self):
         mock_boom = MagicMock()
         mock_boom.side_effect = StandardError('Boom!')
-        self.cli_builder.download_file = mock_boom
+        self.get_cloudify.download_file = mock_boom
 
         args = ArgsObject()
         args.installpip = 'true'
         args.force = 'false'
-        installer = self.cli_builder.CloudifyInstaller(args)
+        installer = self.get_cloudify.CloudifyInstaller(args)
         try:
             installer.install()
             self.fail('installation did not trigger error as expected')
-        except InstallerError as e:
+        except SystemExit as e:
             self.assertEqual(e.message, 'failed downloading pip. '
                                         'reason: Boom!')
 
     def test_install_pip_fail(self):
-        self.cli_builder.download_file = MagicMock(return_value=None)
+        self.get_cloudify.download_file = MagicMock(return_value=None)
 
         args = ArgsObject()
         args.pythonpath = 'non_existing_path'
-        installer = self.cli_builder.CloudifyInstaller(args)
+        installer = self.get_cloudify.CloudifyInstaller(args)
         try:
             installer.install_pip()
             self.fail('installation did not trigger error as expected')
-        except InstallerError as e:
+        except SystemExit as e:
             self.assertEqual(e.message, 'pip installation failed. reason: '
                                         'failed executing command '
                                         '\'non_existing_path get-pip.py\'')
 
     def test_make_virtualenv_fail(self):
         try:
-            self.cli_builder.make_virtualenv('/path/to/dir',
-                                             'non_existing_path')
+            self.get_cloudify.make_virtualenv('/path/to/dir',
+                                              'non_existing_path')
             self.fail('installation did not trigger error as expected')
-        except InstallerError as e:
+        except SystemExit as e:
             self.assertEqual(e.message, 'failed creating virtualenv '
                                         '/path/to/dir. reason failed '
                                         'executing command \'virtualenv -p '
@@ -119,43 +116,43 @@ class CliBuilderUnitTests(unittest.TestCase):
 
     def test_install_non_existing_module(self):
         try:
-            self.cli_builder.install_module('nonexisting_module')
+            self.get_cloudify.install_module('nonexisting_module')
             self.fail('installation did not trigger error as expected')
-        except InstallerError as e:
+        except SystemExit as e:
             self.assertEqual(e.message, 'module \'nonexisting_module\' '
                                         'installation failed. reason failed '
                                         'executing command \'pip install '
                                         'nonexisting_module\'')
 
     def test_get_os_props(self):
-        os = self.cli_builder.get_os_props()[0]
+        os = self.get_cloudify.get_os_props()[0]
         supported_os_list = ('windows', 'linux', 'darwin')
         if os.lower() not in supported_os_list:
             self.fail('os prop \'{0}\' should be equal to one of these names: '
                       '{1}'.format(os, supported_os_list))
 
     def test_download_file(self):
-        self.cli_builder.VERBOSE = True
+        self.get_cloudify.VERBOSE = True
         tmp_file = tempfile.NamedTemporaryFile(delete=True)
-        self.cli_builder.download_file('http://www.google.com', tmp_file.name)
+        self.get_cloudify.download_file('http://www.google.com', tmp_file.name)
         with open(tmp_file.name) as f:
             content = f.readlines()
             self.assertIsNotNone(content)
 
     def test_args_parser(self):
-        self.cli_builder.OS = 'linux'
-        linux_args = self.cli_builder.parse_args([])
+        self.get_cloudify.OS = 'linux'
+        linux_args = self.get_cloudify.parse_args([])
         self.assertEqual(linux_args.pythonpath, 'python',
                          'wrong default python path {} set for linux'
                          .format(linux_args.pythonpath))
 
-        self.cli_builder.OS = 'windows'
-        win_args = self.cli_builder.parse_args([])
+        self.get_cloudify.OS = 'windows'
+        win_args = self.get_cloudify.parse_args([])
         self.assertEqual(win_args.pythonpath, 'c:/python27/python.exe',
                          'wrong default python path {} set for windows'
                          .format(win_args.pythonpath))
 
-        default_args = self.cli_builder.parse_args([])
+        default_args = self.get_cloudify.parse_args([])
         self.assertFalse(default_args.force)
         self.assertFalse(default_args.forceonline)
         self.assertFalse(default_args.installpip)
@@ -168,15 +165,15 @@ class CliBuilderUnitTests(unittest.TestCase):
         self.assertIsNone(default_args.virtualenv)
         self.assertEqual(default_args.wheelspath, 'wheelhouse')
 
-        self.cli_builder.OS = 'linux'
-        set_args = self.cli_builder.parse_args(['-f',
-                                                '--forceonline',
-                                                '--installpip',
-                                                '--virtualenv=venv_path',
-                                                '--quiet',
-                                                '--version=3.2',
-                                                '--installpip',
-                                                '--installpythondev'])
+        self.get_cloudify.OS = 'linux'
+        set_args = self.get_cloudify.parse_args(['-f',
+                                                 '--forceonline',
+                                                 '--installpip',
+                                                 '--virtualenv=venv_path',
+                                                 '--quiet',
+                                                 '--version=3.2',
+                                                 '--installpip',
+                                                 '--installpythondev'])
 
         self.assertTrue(set_args.force)
         self.assertTrue(set_args.forceonline)
@@ -187,14 +184,14 @@ class CliBuilderUnitTests(unittest.TestCase):
 
         # test with args that do not go together
         try:
-            self.cli_builder.parse_args(['--version', '--pre'])
+            self.get_cloudify.parse_args(['--version', '--pre'])
             self.fail('args {} iare expected to raise exception'
                       .format(['--version', '--pre']))
         except BaseException as e:
             print e.message
 
         try:
-            self.cli_builder.parse_args(['--verbose', '--quiet'])
+            self.get_cloudify.parse_args(['--verbose', '--quiet'])
             self.fail('args {} iare expected to raise exception'
                       .format(['--verbose', '--quiet']))
         except BaseException as e:
