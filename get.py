65248216de8cf516acc77e625ae8e6f71ac7ae3f
nir0s <nir36g@gmail.com>
2015-04-21 07:45:54 +0300

    trying to add centos 7 agent

--- get.py
+++ get.py
@@ -62,6 +62,11 @@ def get_centos_final_agent(download=False):
     create_agent(package, download)
 
 
+def get_centos_maipo_agent(download=False):
+    package = get_conf('centos-Maipo-agent')
+    create_agent(package, download)
+
+
 def get_debian_jessie_agent(download=False):
     package = get_conf('debian-jessie-agent')
     create_agent(package, download)
