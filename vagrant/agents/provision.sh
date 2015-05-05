b7d64576e5f27489fcf2955dc374548c0b6a064f
opencm <opencm@gigaspaces.com>
2015-05-05 17:52:15 +0300

    copy license from /cloudify-packager folder

--- vagrant/agents/provision.sh
+++ vagrant/agents/provision.sh
@@ -148,7 +148,7 @@ if [ "${COMMERCIAL}" == "True" ]; then
 	install_module "cloudify-vsphere-plugin" "${AGENT_VENV}" "${PLUGINS_TAG_NAME}" "${GITHUB_USERNAME}" "${GITHUB_PASSWORD}" &&
 	install_module "cloudify-softlayer-plugin" "${AGENT_VENV}" "${PLUGINS_TAG_NAME}" "${GITHUB_USERNAME}" "${GITHUB_PASSWORD}" &&
 	# copy license to virtualenv
-	wget https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/${CORE_TAG_NAME}/docker/cloudify-ui/LICENSE -P "${AGENT_VENV}"
+	cp -f docker/cloudify-ui/LICENSE "${AGENT_VENV}"
 fi
 install_manager_modules "cloudify-manager" "${AGENT_VENV}" "${CORE_TAG_NAME}" &&
 
