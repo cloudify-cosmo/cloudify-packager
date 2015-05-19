8ae9a7524decf51c32afdf2e4dc196f73b5c2e86
limor-gs <limor@gigaspaces.com>
2015-05-19 11:33:38 +0300

    fix cloudify-script-plugin tag

--- vagrant/cli/windows/provision.sh
+++ vagrant/cli/windows/provision.sh
@@ -1,4 +1,5 @@
 export CORE_TAG_NAME="master"
+export PLUGINS_TAG_NAME="master"
 export VERSION=`cat packaging/VERSION  | grep version | sed 's/"version": "//g' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`
 
 echo "VERSION=$VERSION"
@@ -9,7 +10,7 @@ pip wheel --wheel-dir packaging/source/wheels https://github.com/cloudify-cosmo/
 https://github.com/cloudify-cosmo/cloudify-rest-client/archive/$CORE_TAG_NAME.zip#egg=cloudify-rest-client \
 https://github.com/cloudify-cosmo/cloudify-dsl-parser/archive/$CORE_TAG_NAME.zip#egg=cloudify-dsl-parser \
 https://github.com/cloudify-cosmo/cloudify-plugins-common/archive/$CORE_TAG_NAME.zip#egg=cloudify-plugins-common \
-https://github.com/cloudify-cosmo/cloudify-script-plugin/archive/$CORE_TAG_NAME.zip#egg=cloudify-script-plugin
+https://github.com/cloudify-cosmo/cloudify-script-plugin/archive/$PLUGINS_TAG_NAME.zip#egg=cloudify-script-plugin
 
 export VERSION_FILE=$(cat packaging/VERSION)
 
