c561fe411c5c0861dc74f90ea8ca8393e8bf6626
nir0s <nir36g@gmail.com>
2015-06-28 20:29:34 +0300

    fix

--- vagrant/agents/provision.sh
+++ vagrant/agents/provision.sh
@@ -26,6 +26,9 @@ function install_pip
 	curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
 }
 
+GITHUB_USERNAME=$1
+GITHUB_PASSWORD=$2
+
 install_deps
 
 cd ~
@@ -36,8 +39,8 @@ sudo pip install boto==2.36.0 &&
 sudo rm -rf ~/.cache
 
 # clone commercial plugins. this should be a feature in the agent-packager
-git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/cloudify-cosmo/cloudify-vsphere-plugin.git /tmp
-git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/cloudify-cosmo/cloudify-softlayer-plugin.git /tmp
+git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/cloudify-cosmo/cloudify-vsphere-plugin.git /tmp/cloudify-vsphere-plugin
+git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/cloudify-cosmo/cloudify-softlayer-plugin.git /tmp/cloudify-softlayer-plugin
 
 
 # REPLACE branch before production
