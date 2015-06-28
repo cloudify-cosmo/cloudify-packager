decd61db26b3d5f52b37d1c7e719506c286d1e24
Michael Sverdlik <m1keil@users.noreply.github.com>
2015-06-28 16:46:27 +0300

    CFY-3043 - hardcode docker 1.6.0 + aufs install

--- vagrant/docker_images/provision.sh
+++ vagrant/docker_images/provision.sh
@@ -5,7 +5,18 @@ CORE_TAG_NAME="master"
 
 install_docker()
 {
-  curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+  export DEBIAN_FRONTEND=noninteractive
+  kern_extras="linux-image-extra-$(uname -r) linux-image-extra-virtual"
+  sudo apt-get install -y -q $kern_extras
+  sudo modprobe aufs
+
+  sudo apt-get install -y -q curl ca-certificates
+  sudo apt-get install -y -q apt-transport-https ca-certificates
+
+  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+  echo deb https://get.docker.com/ubuntu docker main | sudo tee /etc/apt/sources.list.d/docker.list
+  sudo apt-get update
+  sudo apt-get install -y lxc-docker-1.6.0
 }
 
 setup_jocker_env()
