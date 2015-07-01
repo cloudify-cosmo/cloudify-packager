3585367f150519b2b89306e3e6189860ab1fa6f5
opencm <limor@gigaspaces.com>
2015-07-01 19:56:04 +0300

    Bump version to 3.3m3

--- docker/vars.py
+++ docker/vars.py
@@ -140,7 +140,7 @@ VARS = {
             "make"
         ],
         "nodejs_latest_url": "http://nodejs.org/dist/node-latest.tar.gz",
-        "ui_package_url": "http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/3.3.0/m2-RELEASE/cloudify-ui_3.3.0-m2-b272_amd64.deb",
+        "ui_package_url": "http://gigaspaces-repository-eu.s3.amazonaws.com/org/cloudify3/3.3.0/m3-RELEASE/cloudify-ui_3.3.0-m3-b273_amd64.deb",
         "ports": ["80"],
     }
 }
