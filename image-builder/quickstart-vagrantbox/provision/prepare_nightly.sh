ec2e6283c8cedf6286156b39b829dc74ec77b81b
Michael Sverdlik <mhsver@gmail.com>
2015-08-03 13:59:32 +0300

    remove duplicated installation of guest additions

--- image-builder/quickstart-vagrantbox/provision/prepare_nightly.sh
+++ image-builder/quickstart-vagrantbox/provision/prepare_nightly.sh
@@ -14,17 +14,6 @@ sudo dpkg-reconfigure -f noninteractive cloud-init
 # disable ttyS0
 echo manual | sudo tee /etc/init/ttyS0.override
 
-# install guest additions
-sudo apt-get install -y dkms module-assistant
-sudo m-a -i prepare
-wget http://download.virtualbox.org/virtualbox/4.3.20/VBoxGuestAdditions_4.3.20.iso
-sudo mkdir -p /mnt/iso
-sudo sudo mount -o loop VBoxGuestAdditions_4.3.20.iso /mnt/iso/
-sudo /mnt/iso/VBoxLinuxAdditions.run
-sudo umount /mnt/iso
-sudo rmdir /mnt/iso
-rm VBoxGuestAdditions_4.3.20.iso
-
 # change hostname
 echo cloudify | sudo -S tee /etc/hostname
 echo 127.0.0.1 cloudify | sudo -S tee -a /etc/hosts
