938235790adabafdae4af28c8f3d50727deaff06
Michael Sverdlik <mhsver@gmail.com>
2015-08-03 16:46:45 +0300

    CFY-3300 forgot sudo

--- image-builder/quickstart-vagrantbox/provision/influxdb_monkeypatch.sh
+++ image-builder/quickstart-vagrantbox/provision/influxdb_monkeypatch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-apt-get update
-apt-get install -y jq
+sudo apt-get update
+sudo apt-get install -y jq
 
-DEST=`docker inspect cfy | jq -r '.[0].Volumes["/opt/influxdb/shared/data"]'`
-rm -r $DEST/*
-docker exec cfy /usr/bin/pkill influxdb
+DEST=`sudo docker inspect cfy | jq -r '.[0].Volumes["/opt/influxdb/shared/data"]'`
+sudo rm -r $DEST/*
+sudo docker exec cfy /usr/bin/pkill influxdb
 curl --fail "http://localhost:8086/db?u=root&p=root" -d "{\"name\": \"cloudify\"}"
