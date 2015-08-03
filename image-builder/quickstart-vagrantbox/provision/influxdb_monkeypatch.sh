dc1b558d60052e14f921b0f3ad68ed156e9a620c
Michael Sverdlik <mhsver@gmail.com>
2015-08-03 17:59:25 +0300

    CFY-3300 add sleep

--- image-builder/quickstart-vagrantbox/provision/influxdb_monkeypatch.sh
+++ image-builder/quickstart-vagrantbox/provision/influxdb_monkeypatch.sh
@@ -6,4 +6,5 @@ sudo apt-get install -y jq
 DEST=`sudo docker inspect cfy | jq -r '.[0].Volumes["/opt/influxdb/shared/data"]'`
 sudo find $DEST -type f -delete
 sudo docker exec cfy /usr/bin/pkill influxdb
+sleep 10
 curl --fail "http://localhost:8086/db?u=root&p=root" -d "{\"name\": \"cloudify\"}"
