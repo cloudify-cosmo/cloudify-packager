ce1efe7a857eb23484d88666310e7e7e1f49c6df
Michael Sverdlik <mhsver@gmail.com>
2015-08-03 17:16:18 +0300

    CFY-3300 change rm to find

--- image-builder/quickstart-vagrantbox/provision/influxdb_monkeypatch.sh
+++ image-builder/quickstart-vagrantbox/provision/influxdb_monkeypatch.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get install -y jq
 
 DEST=`sudo docker inspect cfy | jq -r '.[0].Volumes["/opt/influxdb/shared/data"]'`
-sudo rm -r $DEST/*
+sudo find $DEST -type f -delete
 sudo docker exec cfy /usr/bin/pkill influxdb
 curl --fail "http://localhost:8086/db?u=root&p=root" -d "{\"name\": \"cloudify\"}"
