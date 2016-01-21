947960cb1732f8e21a4c9fa038645afa001af7ec
Dan Kilman <dank@gigaspaces.com>
2016-01-21 13:34:43 +0200

    fix link to get-pip.py

--- system_tests/test_offline_cli_package.py
+++ system_tests/test_offline_cli_package.py
@@ -143,8 +143,7 @@ class TestOfflineCliPackage(TestCliPackage):
         self.logger.info('installing cli...')
 
         self._get_resource(self.cli_package_url, ops='-LO', sudo=True)
-        self._get_resource('https://raw.githubusercontent.com/pypa/'
-                           'pip/master/contrib/get-pip.py',
+        self._get_resource('https://bootstrap.pypa.io/get-pip.py',
                            pipe_command='sudo python2.7 -')
         self._execute_command('pip install virtualenv', sudo=True)
 
