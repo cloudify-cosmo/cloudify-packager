cbb32f342e76bc037b4b4f9f0c134b40a0d623f7
nir0s <nir36g@gmail.com>
2016-02-14 10:19:15 +0200

    CFY-4881 finaliziations

--- common/provision.sh
+++ common/provision.sh
@@ -1,19 +1,19 @@
 
 function print_params() {
 
-    echo "## print common parameters"
+    ctx logger info "Printing common parameters..."
 
     declare -A params=( ["VERSION"]=$VERSION ["PRERELEASE"]=$PRERELEASE ["BUILD"]=$BUILD \
                         ["CORE_TAG_NAME"]=$CORE_TAG_NAME ["PLUGINS_TAG_NAME"]=$PLUGINS_TAG_NAME )
     for param in "${!params[@]}"
     do
-            echo "$param - ${params["$param"]}"
+            ctx logger info "$param - ${params["$param"]}"
     done
 }
 
 function install_common_prereqs () {
 
-    echo "## install common prerequisites"
+    ctx logger info "Installing common prerequisites..."
     if  which yum >> /dev/null; then
         sudo yum -y install openssl
         SUDO="sudo"
@@ -22,14 +22,14 @@ function install_common_prereqs () {
         sudo apt-get -y install openssl
         SUDO="sudo"
     else
-        echo 'probably windows machine'
+        ctx logger info 'Probably a Windows machine, skipping...'
     fi
 }
 
 function create_md5() {
 
     local file_ext=$1
-    echo "## create md5"
+    ctx logger info "Creating md5 checksum file for package..."
     md5sum=$(md5sum -t *.$file_ext) &&
     echo $md5sum | $SUDO tee ${md5sum##* }.md5
 }
@@ -44,7 +44,7 @@ function upload_to_s3() {
     string="PUT\n\n$content_type\n$date\n$acl\n/$AWS_S3_BUCKET/$AWS_S3_PATH/$file"
     signature=$(echo -en "${string}" | openssl sha1 -hmac "${AWS_ACCESS_KEY}" -binary | base64)
 
-    echo "## uploading https://$AWS_S3_BUCKET.s3.amazonaws.com/$AWS_S3_PATH/$file"
+    ctx logger info "Uploading https://$AWS_S3_BUCKET.s3.amazonaws.com/${AWS_S3_PATH}/${file}..."
 
     curl -v -X PUT -T "$file" \
       -H "Host: $AWS_S3_BUCKET.s3.amazonaws.com" \
@@ -56,13 +56,13 @@ function upload_to_s3() {
 }
 
 
-export VERSION="3.4.0"
-export PRERELEASE="m2"
-export BUILD="391"
-export CORE_TAG_NAME="3.4m2"
-export PLUGINS_TAG_NAME="1.3.1"
-export AWS_S3_BUCKET="gigaspaces-repository-eu"
-export AWS_S3_PATH="org/cloudify3/${VERSION}/${PRERELEASE}"
+export VERSION="$(ctx node properties version)"
+export PRERELEASE="$(ctx node properties prerelease)"
+export BUILD="$(ctx node properties build)"
+export CORE_TAG_NAME="$(ctx node properties core_tag_name)"
+export PLUGINS_TAG_NAME="$(ctx node properties plugins_tag_name)"
+export AWS_S3_BUCKET="$(ctx node properties aws_s3_bucket)"
+export AWS_S3_PATH="$(ctx node properties aws_s3_path)"
 
 
 print_params
