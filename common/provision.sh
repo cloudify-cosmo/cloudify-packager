e818d4c0de29fa5d3739f19279c9c31240c2a777
opencm <limor@gigaspaces.com>
2016-08-24 14:19:21 +0300

    Bump version

--- common/provision.sh
+++ common/provision.sh
@@ -59,9 +59,9 @@ function upload_to_s3() {
 
 
 export VERSION="4.0.0"
-export PRERELEASE="m1"
-export BUILD="461"
-export CORE_TAG_NAME="4.0m1"
+export PRERELEASE="m2"
+export BUILD="462"
+export CORE_TAG_NAME="4.0m2"
 export AWS_S3_BUCKET="gigaspaces-repository-eu"
 export AWS_S3_PATH="org/cloudify3/${VERSION}/${PRERELEASE}"
 
--- vagrant/docker_images/provision.sh
+++ vagrant/docker_images/provision.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-CORE_TAG_NAME="4.0m1"
+CORE_TAG_NAME="4.0m2"
 
 
 install_docker()
