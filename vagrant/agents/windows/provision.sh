b9578482f6303713b3b258b45dceff63971d5cba
opencm <limor@gigaspaces.com>
2017-11-20 11:12:20 +0200

    Bump version

--- vagrant/agents/windows/provision.sh
+++ vagrant/agents/windows/provision.sh
@@ -1,4 +1,4 @@
-export CORE_TAG_NAME="4.2"
+export CORE_TAG_NAME="4.3.dev1"
 export PLUGINS_TAG_NAME="1.3"
 
 pip install wheel
--- vagrant/docker_images/provision.sh
+++ vagrant/docker_images/provision.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-CORE_TAG_NAME="4.2"
+CORE_TAG_NAME="4.3.dev1"
 
 
 install_docker()
